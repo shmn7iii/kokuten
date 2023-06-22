bf413d26ad8f737a79f42f35a7b2a9ba28430c1f
Shimamura <46078198+shmn7iii@users.noreply.github.com>
2023-06-22 23:02:34 +0900

    remove: delete all files

--- app/controllers/accounts_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AccountsController < ApplicationController
-  before_action :signed_in?, only: %i[show]
-
-  def show
-    @account = current_user.account
-  end
-end
