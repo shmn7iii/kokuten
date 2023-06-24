8a3ea41b8138c2bbe74a5374c87f58aef29289a8
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-06-25 01:30:18 +0900

    update: enable i18n

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,12 @@ module Kokuten
 
     # Use Sidekiq as ActiveJob queue adapter
     config.active_job.queue_adapter = :sidekiq
+
+    # Set timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # Set i18n
+    config.i18n.default_locale = :ja
   end
 end
