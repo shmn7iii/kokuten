034b220891e089ca1c92225b6915ab63d7dbffe5
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-06-26 00:10:19 +0900

    update: create token in db:seed

--- app/models/token.rb
+++ app/models/token.rb
@@ -5,10 +5,6 @@ class Token < ApplicationRecord
 
   has_many :token_transactions
 
-  after_initialize do
-    self.script_pubkey_payload_hex ||= create_token.script_pubkey.to_payload.bth
-  end
-
   TOKEN_ID = 1 # TODO: .envにでも書きたい
 
   def self.instance
@@ -27,40 +23,5 @@ class Token < ApplicationRecord
     script_pubkey = Tapyrus::Script.parse_from_payload(script_pubkey_payload_hex.htb)
     color_identifier = Tapyrus::Color::ColorIdentifier.reissuable(script_pubkey)
     Glueby::Contract::Token.new(color_id: color_identifier)
-  end
-
-  private
-
-  # 発行者は UTXO Provider とする。1枚作って script_pubkey を作り即座に burn する。
-  # FIXME: administrate で Token のページ見る度に走ってそう。
-  def create_token
-    token, = Glueby::Contract::Token.issue!(issuer: utxo_provider_wallet, amount: 1,
-                                            token_type: Tapyrus::Color::TokenTypes::REISSUABLE)
-    generate
-
-    token.burn!(sender: utxo_provider_wallet, amount: 1)
-    generate
-
-    token
-  end
-
-  # Glueby::UtxoProvider.instance.wallet は Glueby::Internal::Wallet クラスなため使わない。
-  def utxo_provider_wallet
-    Glueby::Wallet.load('UTXO_PROVIDER_WALLET')
-  end
-
-  # XXX:
-  # ActiveRecord Transaction でロックしてるので system 使って外から Rake タスク実行すると怒られるので一時的に generate メソッドを追加
-  def generate
-    utxo_provider_address = Glueby::UtxoProvider.instance.address
-    aggregate_private_key = ENV['TAPYRUS_AUTHORITY_KEY']
-    Glueby::Internal::RPC.client.generatetoaddress(1, utxo_provider_address, aggregate_private_key)
-
-    latest_block_num = Glueby::Internal::RPC.client.getblockcount
-    synced_block = Glueby::AR::SystemInformation.synced_block_height
-    (synced_block.int_value + 1..latest_block_num).each do |height|
-      Glueby::BlockSyncer.new(height).run
-      synced_block.update(info_value: height.to_s)
-    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,29 @@
 # frozen_string_literal: true
 
-Glueby::AR::SystemInformation.create(info_key: 'synced_block_number', info_value: '0') if Glueby::AR::SystemInformation.synced_block_height.nil?
+# Create SystemInformation
+if Glueby::AR::SystemInformation.synced_block_height.nil?
+  Glueby::AR::SystemInformation.create(info_key: 'synced_block_number', info_value: '0')
+end
+
+# Create UTXO Provider
+# glueby_wallet_id が UTXO_PROVIDER_WALLET なことは自明だがインスタンスの生成のため呼び出し
+glueby_wallet_id = Glueby::UtxoProvider.instance.wallet.id
+utxo_provider_wallet = Wallet.create!(glueby_wallet_id:)
+
+# Create Token
+# UTXO Provider に資金を送る
+Glueby::BlockGenerateJob.perform_now
+Glueby::ManageUtxoPoolJob.perform_now
+Glueby::BlockGenerateJob.perform_now
+
+token, = Glueby::Contract::Token.issue!(
+  issuer: utxo_provider_wallet.glueby_wallet,
+  token_type: Tapyrus::Color::TokenTypes::REISSUABLE,
+  amount: 1
+)
+Glueby::BlockGenerateJob.perform_now
+
+token.burn!(sender: utxo_provider_wallet.glueby_wallet, amount: 1)
+Glueby::BlockGenerateJob.perform_now
+
+Token.create!(script_pubkey_payload_hex: token.script_pubkey.to_payload.bth)
