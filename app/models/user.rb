4fbb40a0db1fb2448f13745af7a59882f5e3252e
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-07-04 23:18:27 +0900

    chore: change phone_number to string

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, length: { maximum: 255 }
   validates :email, presence: true, length: { maximum: 255 }
 
   after_initialize do
