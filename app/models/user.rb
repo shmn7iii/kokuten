ea508193cc9a3060706886b0774f44d4e9f39685
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-07-04 10:29:01 +0900

    add: install webauthn-ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,30 @@
 
 # ユーザーモデル
 class User < ApplicationRecord
+  CREDENTIAL_MIN_AMOUNT = 1
+
   validates :username, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :phone_number, presence: true
   validates :email, presence: true, length: { maximum: 255 }
 
+  after_initialize do
+    self.webauthn_id ||= WebAuthn.generate_user_id
+  end
+
   has_one :account
   has_one :wallet
 
+  has_many :credentials
   has_many :wallet_deposit_requests
   has_many :wallet_withdrawal_requests
   has_many :wallet_transfer_requests_as_sender, class_name: 'WalletTransferRequest', foreign_key: 'sender_id'
   has_many :wallet_transfer_requests_as_receiver, class_name: 'WalletTransferRequest', foreign_key: 'receiver_id'
+
+  def can_delete_credentials?
+    credentials.size > CREDENTIAL_MIN_AMOUNT
+  end
 
   def wallet_transfer_requests
     [
